b0dfeb7c1866f2c4712025c9ee579205a9722653
cooper-anderson <cooper@cooperstuff.ninja>
2017-05-17 16:36:31 -0700

    Added powerline sync

---
+++ install_powerline_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pythonPath=$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")
+dir=~/dotfiles
+olddir=~/.dotfiles_old
+
+
+mv $pythonPath/powerline/config_files $olddir/powerline/
+ln -s $dir/powerline/config_files $pythonPath/powerline/config_files
