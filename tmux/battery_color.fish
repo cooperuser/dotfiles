eab3de3b1602819511966e275572afb4200d495a
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-19 16:59:40 -0800

    [tmux] New battery colors

--- tmux/battery_color.fish
+++ tmux/battery_color.fish
@@ -1,10 +1,16 @@
 #!/usr/local/bin/fish
 # if test (cat /sys/class/power_supply/BAT0/status) = Discharging
 	if test (./battery.sh) -lt 26
-		echo -n -s 196
+		echo -n -s 203
 	else
-		echo -n -s 40
+		echo -n -s 113
 	end
 # else
 # 	echo -n -s 190
 # end
+# 	if test (./battery.sh) -lt 26
+		# echo -n -s 196
+	# else
+		# echo -n -s 40
+	# end
+
