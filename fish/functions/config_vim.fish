abe3c4dc9a75fad8ca938e0d96c2e4e9ac678beb
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-30 16:23:14 -0800

    [fish] Change <C-v> to open vim lua files

--- fish/functions/config_vim.fish
+++ fish/functions/config_vim.fish
@@ -3,6 +3,7 @@
 function config_vim
 	set CURRENT_DIRECTORY (pwd);
 	cd ~/.config/nvim;
-	vim ~/.config/nvim/init.vim ~/.config/nvim/config/plugins.vim ~/.config/nvim/config/settings.vim ~/.config/nvim/config/keybinds.vim ~/.config/nvim/config/statusline.vim ~/.config/nvim/config/bufline.vim -c 'bp | bn'
+	vim init.lua lua/ampersand/plugins.lua lua/ampersand/settings.lua lua/ampersand/keybinds.lua -c 'bp | bn'
 	cd $CURRENT_DIRECTORY;
 end
+
