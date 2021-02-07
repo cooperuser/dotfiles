0b37749caf1712d60c1b85a2184b11165bc7d6a0
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-02-07 06:15:53 -0500

    [vim] Set vimtex view method

--- nvim/lua/ampersand/plugins/vimtex.lua
+++ nvim/lua/ampersand/plugins/vimtex.lua
@@ -2,7 +2,8 @@ local plugin = {name = "vimtex"}
 Plugins[plugin.name] = plugin
 
 function plugin.settings()
-	vim.g.vimtex_view_method = "skim"
+	vim.g.vimtex_view_method = "zathura"
+	-- vim.g.vimtex_compiler_method = "tectonic"
 end
 
 return function()
