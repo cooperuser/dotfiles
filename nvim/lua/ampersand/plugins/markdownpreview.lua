f9cf9794c8be4f0d2428b2f903e98a359c21a9d8
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-02-07 06:15:40 -0500

    [vim] Make markdownpreview work on any user

--- nvim/lua/ampersand/plugins/markdownpreview.lua
+++ nvim/lua/ampersand/plugins/markdownpreview.lua
@@ -2,8 +2,10 @@ local plugin = {name = "markdownpreview"}
 Plugins[plugin.name] = plugin
 
 function plugin.settings()
-	vim.g.mkdp_markdown_css = "/Users/ampersand/.config/nvim/plugin/markdownpreview/markdown.css"
-	vim.g.mkdp_highlight_css = "/Users/ampersand/.config/nvim/plugin/markdownpreview/markdown_dark.css"
+	local config = vim.fn["stdpath"]("config") .. "/plugin/markdownpreview"
+	vim.g.mkdp_markdown_css = config .. "/markdown.css"
+	vim.g.mkdp_highlight_css = config .. "/markdown_dark.css"
+	-- vim.g.mkdp_refresh_slow = true
 end
 
 function plugin.keybinds()
